74b44128291801ba57b4c539bed5683d0092a5c3
Mads Mikkelsen <madsmikkelsen99@proton.me>
2024-10-16 19:14:43 +0100

    Added README and changes to spectral_analysis. Added plotting of example and test stars along with Balmer serie

---
+++ README.md
@@ -0,0 +1,5 @@
+### SPECTRAL ANALYSIS 
+
+This script will perform normalisation of a series of test stars along with plotting the spectral lines of interest.
+Firstly it is loading and plotting a chosen example star from the "ExampleStar" folder using a user input in the terminal.
+Next it is plotting a test star with (currently only) the Balmer series from 3900 Å to 6500 Å overlay as red vertical lines.
---
+++ spectral_analysis.py
@@ -0,0 +1,57 @@
+import os, sys, glob
+import numpy as np
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+"""
+TO DO:
+- Perform normalisation on the test spectra
+- Find more spectral lines from the example stars
+- Characterise the test stars from the example stars
+- Normalise the problem stars and characterise them
+"""
+
+
+
+### Seaborn customisation ###
+sns.set_theme(style="darkgrid")
+
+### Paths to example, test and problem stars ###
+example_data = glob.glob("ExampleStars/*.dat")
+test_data = glob.glob("TestStars/*.dat")
+
+### Choose which stellar spectrum to plot ###
+file_index_example = int(input(f"\nEnter the index of the file you want to plot (0-{len(example_data)-1}): "))
+file_index_test = int(input(f"\nEnter the index of the file you want to plot (0-{len(test_data)-1}): "))
+
+plt.figure(num=1001)
+
+selected_example = example_data[file_index_example]
+example_star = pd.read_csv(selected_example, sep='\t', header=None, names=["Wavelength", "Flux"])
+
+sns.lineplot(x="Wavelength", y="Flux", data=example_star)
+plt.title(f"Spectrum from {os.path.basename(selected_example)}")
+plt.xlabel("Wavelength [A]")
+plt.ylabel(r"Normalised Flux [erg/cm$^2$/A/s]")
+
+plt.figure(num=2001)
+
+selected_test = test_data[file_index_test]
+test_star = pd.read_csv(selected_test, sep='\t', header=None, names=["Wavelength", "Flux"])
+
+sns.lineplot(x="Wavelength", y="Flux", data=test_star)
+plt.title(f"Spectrum from {os.path.basename(selected_test)}")
+plt.xlabel("Wavelength [A]")
+plt.ylabel(r"Normalised Flux [erg/cm$^2$/A/s]")
+
+### Plotting vertical lines at spectral lines of interest ###
+    ### Balmer series ###
+wavel = [6562.79, 4861.35, 4340.472, 4101.734, 3970.075]
+for wl in wavel:
+    plt.axvline(x=wl, color="red", linestyle="dotted", alpha=0.5)
+
+### Normalisation of test spectrum ###
+
+plt.close(1001)
+plt.show()
