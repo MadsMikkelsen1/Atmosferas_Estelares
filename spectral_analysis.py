290be3212268632cb89bba3a74207e52d5a7a5bd
Mads Mikkelsen <madsmikkelsen99@proton.me>
2024-10-25 23:44:50 +0100

    added spectral lines

--- spectral_analysis.py
+++ spectral_analysis.py
@@ -3,6 +3,7 @@ import numpy as np
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
+import normPlot
 
 """
 TO DO:
@@ -37,6 +38,9 @@ plt.ylabel(r"Normalised Flux [erg/cm$^2$/A/s]")
 
 plt.figure(num=2001)
 
+### Normalisation of test spectrum ###
+#normPlot.normplot("/Users/madsmikkelsen/ULL/1. Semester/Atmosferas_Estelares/ProblemStar1.dat")
+
 selected_test = test_data[file_index_test]
 test_star = pd.read_csv(selected_test, sep='\t', header=None, names=["Wavelength", "Flux"])
 
@@ -47,11 +51,15 @@ plt.ylabel(r"Normalised Flux [erg/cm$^2$/A/s]")
 
 ### Plotting vertical lines at spectral lines of interest ###
     ### Balmer series ###
-wavel = [6562.79, 4861.35, 4340.472, 4101.734, 3970.075]
-for wl in wavel:
-    plt.axvline(x=wl, color="red", linestyle="dotted", alpha=0.5)
+wavel_balmer = [6562.79, 4861.35, 4340.472, 4101.734, 3970.075]
+for wl in wavel_balmer:
+    plt.axvline(x = wl, color = "red", linestyle = "dotted", alpha = 0.5, label = "Balmer Series")
+plt.axvline(x = 4471, color = "yellow", linestyle = "dotted", alpha = 0.5, label = "He I")
+plt.axvline(x = 4121, color = "teal", linestyle = "dotted", alpha = 0.5, label = "He I")
+plt.axvline(x = 4089, color = "orange", linestyle = "dotted", alpha = 0.5, label = "Si IV")
+plt.axvline(x = 4552, color = "black", linestyle = "dotted", alpha = 0.5, label = "Si III")
+plt.axvline(x = 4226, color = "purple", linestyle = "dotted", alpha = 0.5, label = "Ca I")
 
-### Normalisation of test spectrum ###
-
-plt.close(1001)
+#plt.close(1001)
+plt.legend()
 plt.show()
