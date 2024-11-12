318bfdae6c5c14609b1d5616dcbdf2bb47d4e33a
Mads Mikkelsen <Mads@mik86.dk>
2024-11-12 11:27:14 +0000

    Delete normPlot_test directory

--- normPlot_test/Normalisation_test.py
+++
@@ -1,9 +0,0 @@
-import os, sys, glob
-import numpy as np
-import seaborn as sns
-import pandas as pd
-import matplotlib.pyplot as plt
-import normPlot
-
-
-normPlot.normplot('/Users/madsmikkelsen/ULL/1. Semester/Atmosferas_Estelares/ProblemStar1.s')
